9407f4872d7c6d9c0c5b0de3368e5bde7caeda2e
michalkaminski <m.kaminski@clawrock.com>
2024-06-13 14:09:02 +0200

    Create movies model

---
+++ app/Models/Movie.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Movie extends Model
+{
+    protected $fillable = [
+        'title',
+        'tmdb_id',
+        'original_title',
+        'release_date',
+        'poster_path',
+        'vote_average',
+        'vote_count',
+        'overview',
+        'original_language'
+    ];
+
+    use HasFactory;
+}
