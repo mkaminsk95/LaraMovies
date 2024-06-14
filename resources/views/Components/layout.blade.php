edca049a96c210f6c95cb2093c6347e0c02a10b8
michalkaminski <m.kaminski@clawrock.com>
2024-06-14 15:34:38 +0200

    Apply dark mode

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="en" class="h-full bg-gray-100">
+<html lang="en" class="bg-gray-100 dark:bg-gray-900">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
@@ -8,8 +8,8 @@
     <title>About page</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body class="h-full">
-<header class="bg-white">
+<body>
+<header class="bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-600">
     <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
         <div class="flex lg:flex-1">
             <a href="#" class="-m-1.5 p-1.5">
@@ -28,12 +28,12 @@
             </button>
         </div>
         <div class="hidden lg:flex lg:gap-x-12">
-            <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Movies</a>
-            <a href="#" class="text-sm font-semibold leading-6 text-gray-900">About</a>
-            <a href="#" class="text-sm font-semibold leading-6 text-gray-900">News</a>
+            <a href="#" class="text-sm font-semibold leading-6">Movies</a>
+            <a href="#" class="text-sm font-semibold leading-6">About</a>
+            <a href="#" class="text-sm font-semibold leading-6">News</a>
         </div>
         <div class="hidden lg:flex lg:flex-1 lg:justify-end">
-            <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Log in <span
+            <a href="#" class="text-sm font-semibold leading-6">Log in <span
                     aria-hidden="true">&rarr;</span></a>
         </div>
     </nav>
--- resources/views/list-movies.blade.php
+++ resources/views/list-movies.blade.php
@@ -1,8 +1,8 @@
 <x-layout>
     <div class="mt-12 mx-auto max-w-lg flex justify-center flex-col">
-        <ul role="list" class="divide-y-4 divide-gray-100 bg-white rounded">
+        <ul role="list" class="divide-y-4 divide-gray-100 dark:divide-gray-900 rounded">
             @foreach($paginatedMovies as $movie)
-                <li class="flex gap-x-6">
+                <li class="flex gap-x-6 bg-white dark:bg-gray-800">
                     <div class="flex min-w-0 gap-x-4">
                         <img class="h-30 w-20 flex-none rounded bg-gray-50"
                              src="https://image.tmdb.org/t/p/w1280/{{ $movie['poster_path'] }}"
@@ -10,10 +10,10 @@
                     </div>
                     <div class="flex flex-row flex-1 justify-between shrink-0 mt-3 rounded">
                         <div class="min-w-0">
-                            <span class="text-sm font-semibold leading-6 text-gray-900">{{ $movie['title'] }}</span>
-                            <span class="mt-1 truncate text-xs leading-5 text-gray-500">{{ $movie['release_date']}}</span>
+                            <span class="text-sm font-semibold leading-6 text-gray-900 dark:text-gray-600">{{ $movie['title'] }}</span>
+                            <span class="mt-1 truncate text-xs leading-5 text-gray-500 dark:text-gray-700">{{ $movie['release_date']}}</span>
                         </div>
-                        <span class="text-sm leading-6 text-gray-900 pr-5">Drama</span>
+                        <span class="text-sm leading-6 text-gray-900 dark:text-gray-600 pr-5">Drama</span>
                     </div>
                 </li>
             @endforeach
