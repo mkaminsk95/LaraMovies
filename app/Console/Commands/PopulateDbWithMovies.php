db35b21cc1e83a897ed1d62bbfc5695933969b2f
michalkaminski <m.kaminski@clawrock.com>
2024-06-14 09:03:17 +0200

    Modify command and service so it takes n number of movies

--- app/Console/Commands/PopulateDbWithMovies.php
+++ app/Console/Commands/PopulateDbWithMovies.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace App\Console\Commands;
 
+use App\Services\MovieServiceInterface;
 use Illuminate\Console\Command;
 use App\Models\Movie;
 use App\Services\TMDBService;
@@ -14,7 +15,7 @@ class PopulateDbWithMovies extends Command
      *
      * @var string
      */
-    protected $signature = 'app:populate-db-with-movies';
+    protected $signature = 'app:populate-db-with-movies {number : The number of movies to fetch}';
 
     /**
      * The console command description.
@@ -23,26 +24,44 @@ class PopulateDbWithMovies extends Command
      */
     protected $description = 'Command for populating the database with top rated movies from external source.';
 
+    public function __construct(
+        private readonly MovieServiceInterface $movieService
+    )
+    {
+        parent::__construct();
+    }
+
+
     /**
      * Execute the console command.
      */
     public function handle(): void
     {
-        $service = new TMDBService();
-        $movies = $service->getTopRatedMovies()['movies'];
+        $number = $this->argument('number');
+
+        if (!is_numeric($number)) {
+            $this->error('The number must be an integer.');
+            return;
+        }
+
+        $movies = $this->movieService->getMovies((int)$number);
 
         foreach ($movies as $movie) {
-            Movie::create([
-                'tmdb_id' => $movie['id'],
-                'title' => $movie['title'],
-                'original_title' => $movie['original_title'],
-                'release_date' => $movie['release_date'],
-                'poster_path' => $movie['poster_path'],
-                'vote_average' => $movie['vote_average'],
-                'vote_count' => $movie['vote_count'],
-                'overview' => $movie['overview'],
-                'original_language' => $movie['original_language'],
-            ]);
+            try {
+                Movie::create([
+                    'tmdb_id' => $movie['id'],
+                    'title' => $movie['title'],
+                    'original_title' => $movie['original_title'],
+                    'release_date' => $movie['release_date'],
+                    'poster_path' => $movie['poster_path'],
+                    'vote_average' => $movie['vote_average'],
+                    'vote_count' => $movie['vote_count'],
+                    'overview' => $movie['overview'],
+                    'original_language' => $movie['original_language'],
+                ]);
+            } catch (\Illuminate\Database\QueryException $e) {
+                continue;
+            }
         }
     }
 }
--- app/Services/TMDBService.php
+++ app/Services/TMDBService.php
@@ -6,19 +6,35 @@ namespace App\Services;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Http;
 
-class TMDBService
+class TMDBService implements MovieServiceInterface
 {
     const API_URL = 'https://api.themoviedb.org/3/movie/top_rated';
 
-    public function getTopRatedMovies(int $page = 1): array
+    public function getMovies(int $n = 10): array
     {
         $apiKey = Config::get('services.themoviedb.api_key');
 
-        $response = Http::get($this::API_URL, [
-            'api_key' => $apiKey,
-            'page' => $page,
-        ]);
+        $page = 1;
+        $movies = [];
+        while ($n > 0) {
+            $result = Http::get($this::API_URL,
+                [
+                    'api_key' => $apiKey,
+                    'page' => $page,
+                ]
+            )->json()['results'];
 
-        return ['movies' => $response->json()['results']];
+            if ($n > count($result)) {
+                $movies = array_merge($result, $movies);
+                $n -= count($result);
+            } else {
+                $movies = array_merge($result, array_slice($movies, 0, $n));
+                $n = 0;
+            }
+
+            $page++;
+        }
+
+        return $movies;
     }
 }
