e3c22dd7ff218f9119a4e86aa0774f7c60fe6829
michalkaminski <m.kaminski@clawrock.com>
2024-06-14 09:40:51 +0200

    Refactoring

--- app/Services/TMDBService.php
+++ app/Services/TMDBService.php
@@ -9,32 +9,49 @@ use Illuminate\Support\Facades\Http;
 class TMDBService implements MovieServiceInterface
 {
     const API_URL = 'https://api.themoviedb.org/3/movie/top_rated';
+    private string $apiKey;
+
+    public function __construct()
+    {
+        $this->apiKey = (string)Config::get('services.themoviedb.api_key');
+    }
 
     public function getMovies(int $n = 10): array
     {
-        $apiKey = Config::get('services.themoviedb.api_key');
-
         $page = 1;
         $movies = [];
-        while ($n > 0) {
-            $result = Http::get($this::API_URL,
-                [
-                    'api_key' => $apiKey,
-                    'page' => $page,
-                ]
-            )->json()['results'];
 
-            if ($n > count($result)) {
-                $movies = array_merge($result, $movies);
-                $n -= count($result);
-            } else {
-                $movies = array_merge($result, array_slice($movies, 0, $n));
-                $n = 0;
-            }
+        while ($n > 0) {
+            $result = $this->fetchMovies($page);
+            $movies = $this->mergeMovies($movies, $result, $n);
 
             $page++;
         }
 
         return $movies;
+    }
+
+    private function mergeMovies(array $movies, array $result, int &$n): array
+    {
+        $count = count($result);
+        if ($n >= $count) {
+            $movies = array_merge($movies, $result);
+            $n -= count($result);
+        } else {
+            $movies = array_merge($movies, array_slice($result, 0, $n));
+            $n = 0;
+        }
+
+        return $movies;
+    }
+
+    private function fetchMovies(int $page): array
+    {
+        $response = Http::get(self::API_URL, [
+            'api_key' => $this->apiKey,
+            'page' => $page,
+        ]);
+
+        return $response->json()['results'] ?? [];
     }
 }
