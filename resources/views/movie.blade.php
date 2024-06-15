fe55cd63168345bdff672285fba8779127653c91
michalkaminski <m.kaminski@clawrock.com>
2024-06-15 11:30:46 +0200

    Add movie page

---
+++ resources/views/movie.blade.php
@@ -0,0 +1,23 @@
+<x-layout>
+    <div class="flex flex-col mx-auto mt-10 w-1/2">
+        <div class="relative">
+            <img src="https://image.tmdb.org/t/p/original/{{ $movie['backdrop_path'] }}" alt="poster">
+            <div class="h-full w-full absolute -translate-y-full text-white bg-gradient-to-t from-gray-900 to-transparent ">
+            </div>
+            <div class="relative  left-16 -translate-y-full text-white">
+                <span class="text-4xl">{{ $movie['title'] }}</span>
+                <span class="text-2xl">({{ (new DateTime($movie['release_date']))->format('Y') }})</span>
+            </div>
+        </div>
+        <div class="flex flex-row px-8 py-8 bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-400">
+            <div class="basis-1/4">
+                <img class="flex-none rounded bg-gray-50"
+                     src="https://image.tmdb.org/t/p/w1280/{{ $movie['poster_path'] }}"
+                     alt="">
+            </div>
+            <div class="basis-1/2 ml-12">
+                <p class="text-l mt-4">{{ $movie['overview'] }}</p>
+            </div>
+        </div>
+    </div>
+</x-layout>
