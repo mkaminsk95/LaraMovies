40954e116932b384eb57cbf1f1e58c6fad3f5853
michalkaminski <m.kaminski@clawrock.com>
2024-06-16 11:03:48 +0200

    Add genre migration

---
+++ database/migrations/2024_06_15_173400_create_movie_genre_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('genres', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name')->unique();
+        });
+        Schema::create('movie_genre', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('movie_id')->constrained()->onDelete('cascade');
+            $table->foreignId('genre_id')->constrained()->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('movie_genre');
+        Schema::dropIfExists('genres');
+    }
+};
