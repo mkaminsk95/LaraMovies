e86c5a581b26541807d40d31edc3a0b7f9966cbc
michalkaminski <m.kaminski@clawrock.com>
2024-06-16 11:14:17 +0200

    Modify movie page

--- resources/views/movie.blade.php
+++ resources/views/movie.blade.php
@@ -4,9 +4,14 @@
             <img src="https://image.tmdb.org/t/p/original/{{ $movie['backdrop_path'] }}" alt="poster">
             <div class="h-full w-full absolute -translate-y-full text-white bg-gradient-to-t from-gray-900 to-transparent ">
             </div>
-            <div class="relative  left-16 -translate-y-full text-white">
+            <div class=" relative left-16 -translate-y-full text-white">
                 <span class="text-4xl">{{ $movie['title'] }}</span>
                 <span class="text-2xl">({{ (new DateTime($movie['release_date']))->format('Y') }})</span>
+                <div class="mt-3">
+                    @foreach($movie['genres'] as $genre)
+                        <span class="text-l p-1 mr-3 bg-gray-800 ">{{ $genre['name'] }}</span>
+                    @endforeach
+                </div>
             </div>
         </div>
         <div class="flex flex-row px-8 py-8 bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-400">
