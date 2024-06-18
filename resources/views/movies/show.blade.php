7e9263e2398f647e32aa0680e7cef9cd15217e78
michalkaminski <m.kaminski@clawrock.com>
2024-06-18 20:20:25 +0200

    Implement dummy image for no image

--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -1,7 +1,11 @@
 <x-layout>
     <div class="flex flex-col mx-auto mt-10 w-1/2">
         <div class="relative">
-            <img src="https://image.tmdb.org/t/p/original/{{ $movie['backdrop_path'] }}" alt="poster">
+            @if($movie['backdrop_path'] !== null)
+                <img src="https://image.tmdb.org/t/p/w1280/{{ $movie['backdrop_path'] }}" alt="poster">
+            @else
+                <img src="https://dummyimage.com/1280x720/d2d5db/fff&text=+" alt="poster">
+            @endif
             <div class="h-full w-full absolute -translate-y-full text-white bg-gradient-to-t from-gray-900 to-transparent ">
             </div>
             <div class=" relative left-16 -translate-y-full text-white">
@@ -16,9 +20,14 @@
         </div>
         <div class="flex flex-row px-8 py-8 bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-400">
             <div class="basis-1/4">
-                <img class="flex-none rounded bg-gray-50"
-                     src="https://image.tmdb.org/t/p/w1280/{{ $movie['poster_path'] }}"
-                     alt="">
+                @if($movie['poster_path'] !== null)
+                    <img class="flex-none rounded bg-gray-50"
+                         src="https://image.tmdb.org/t/p/w780{{ $movie['poster_path'] }}"
+                         alt="">
+                @else
+                    <img src="https://dummyimage.com/780x1170/d2d5db/fff&text=+" alt="poster">
+                @endif
+
             </div>
             <div class="basis-1/2 ml-12">
                 <p class="text-l mt-4">{{ $movie['overview'] }}</p>
