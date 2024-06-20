242e6df966fc572e201b303e4da058e6f564b7f6
michalkaminski <m.kaminski@clawrock.com>
2024-06-20 20:27:17 +0200

    Create country select component

---
+++ app/View/Components/CountrySelect.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+
+namespace App\View\Components;
+
+use Illuminate\View\Component;
+
+class CountrySelect extends Component
+{
+    public $label;
+    public $selected;
+
+    public function __construct($label, $selected = null)
+    {
+        $this->label = $label;
+        $this->selected = $selected;
+    }
+
+    public function render()
+    {
+        return view('components.country-select');
+    }
+
+    public function getMovieLanguages(): array
+    {
+        return config('movie_languages');
+    }
+}
