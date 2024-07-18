b3986f8f2e2bc6e60230b8abdaaa2d9657d50b17
michalkaminski <m.kaminski@clawrock.com>
2024-07-18 14:10:09 +0200

    Create actors populator command

---
+++ app/Console/Commands/PopulateDbWithActors.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Console\Commands;
+
+use App\Services\ActorsPopulatorInterface;
+use Illuminate\Console\Command;
+
+class PopulateDbWithActors extends Command
+{
+    protected $signature = 'app:populate-db-with-actors';
+    protected $description = 'Command for populating the database with actors for existing movies.';
+
+    public function __construct(
+        private readonly ActorsPopulatorInterface $actorsPopulator
+    )
+    {
+        parent::__construct();
+    }
+
+    public function handle(): void
+    {
+        try {
+            $this->actorsPopulator->populateActors();
+        } catch (\Exception $e) {
+            $this->error($e->getMessage());
+        }
+
+        $this->info('Actors have been successfully populated.');
+    }
+}
