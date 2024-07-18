9712899bf1a99b65de042f7ef24a5421ae148836
michalkaminski <m.kaminski@clawrock.com>
2024-07-18 14:12:17 +0200

    Add carousel to movie page

--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -51,5 +51,21 @@
             :isFavourite="$isFavourite"
             :isWatchlistItem="$isWatchlistItem"
         />
+
+        @if($movie->credits->count())
+            <h2 class="pl-[80px] pt-4 dark:text-gray-300 text-2xl font-medium tracking-wider">Top Billed Cast</h2>
+            <x-carousel class="w-full md:w-[825px] lg:w-[1000px] px-14 pb-8">
+                @foreach($movie->credits as $credit)
+                    @if($credit->person['profile_path'] !== null)
+                        <li class="glide__slide">
+                            <img class="border dark:border-gray-700" src="https://image.tmdb.org/t/p/w185{{ $credit->person['profile_path'] }}" alt="">
+                            <p class="pt-5 text-center text-base dark:text-gray-400">{{ $credit['character'] }}</p>
+                            <p class="pt-2 text-center text-sm text-gray-600">{{ $credit->person['name'] }}</p>
+                        </li>
+                    @endif
+                @endforeach
+            </x-carousel>
+        @endif
+
     </div>
 </x-layout>
