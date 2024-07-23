b5b3db4baacc9fd96c49deb1289b079beea5e14c
michalkaminski <m.kaminski@clawrock.com>
2024-07-23 10:06:56 +0200

    Include carousel.js file in vite config

--- resources/js/carousel.js
+++ resources/js/carousel.js
@@ -2,7 +2,6 @@ import Glide from '@glidejs/glide';
 import "@glidejs/glide/dist/css/glide.core.min.css";
 import "@glidejs/glide/dist/css/glide.theme.min.css";
 
-
 document.addEventListener('DOMContentLoaded', () => {
     new Glide('.glide', {
         type: 'slider',
