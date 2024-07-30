fc154b7710c17432ef8615338c93462f76afc9ae
michalkaminski <m.kaminski@clawrock.com>
2024-07-30 18:45:01 +0200

    Modify carousel

--- resources/js/carousel.js
+++ resources/js/carousel.js
@@ -3,19 +3,45 @@ import "@glidejs/glide/dist/css/glide.core.min.css";
 import "@glidejs/glide/dist/css/glide.theme.min.css";
 
 document.addEventListener('DOMContentLoaded', () => {
-    new Glide('.glide', {
-        type: 'slider',
-        startAt: 0,
-        perView: 6.4,
-        bound: true,
-        gap: 30,
-        breakpoints: {
-            768: {
-                perView: 4.7
-            },
-            1024: {
-                perView: 5.6
+    const carouselElements = document.querySelectorAll('.glide');
+
+    carouselElements.forEach((carouselElement) => {
+
+        let perViewExtraSmall = 3 , perViewSmall = 4.7, perViewMedium = 5.6, perViewLarge = 8.4;
+
+        if (carouselElement) {
+            const settings = JSON.parse(carouselElement.getAttribute('data-carousel-settings'));
+            if (settings) {
+                perViewExtraSmall = settings.perViewExtraSmall != null ? settings.perViewExtraSmall : perViewExtraSmall;
+                perViewSmall = settings.perViewSmall != null ? settings.perViewSmall : perViewSmall;
+                perViewMedium = settings.perViewMedium != null ? settings.perViewMedium : perViewMedium;
+                perViewLarge = settings.perViewLarge != null ? settings.perViewLarge : perViewLarge;
             }
         }
-    }).mount();
+
+        const glide = new Glide(carouselElement, {
+            type: 'slider',
+            startAt: 0,
+            perView: perViewLarge,
+            bound: true,
+            gap: 30,
+            breakpoints: {
+                640: {
+                    perView: perViewExtraSmall,
+                    gap: 15
+                },
+                768: {
+                    perView: perViewSmall,
+                    gap: 20
+                },
+                1024: {
+                    perView: perViewMedium,
+                    gap: 20
+                }
+            }
+        });
+        glide.mount();
+
+        window.dispatchEvent(new Event('glidemounted'));
+    });
 });
--- resources/views/Components/carousel.blade.php
+++ resources/views/Components/carousel.blade.php
@@ -1,20 +1,35 @@
-@vite(['resources/js/carousel.js'])
-<div {{ $attributes }}>
-    <div class="glide mt-5">
+@props(['perViewExtraSmall' => 3, 'perViewSmall' => 4.7, 'perViewMedium' => 5.6, 'perViewLarge' => 8.4])
+@vite('resources/js/carousel.js')
+<div {{ $attributes }} x-data="window.initializeCarousel()" @glidemounted.window="topValue = calculateHeight()" @resize.window="topValue = calculateHeight()">
+    <div class="glide mt-5" data-carousel-settings='{"perViewExtraSmall": {{ $perViewExtraSmall }}, "perViewSmall": {{ $perViewSmall }}, "perViewMedium": {{ $perViewMedium }}, "perViewLarge": {{ $perViewLarge }}}'>
         <div class="glide__track" data-glide-el="track">
             <ul class="glide__slides">
                 {{ $slot }}
             </ul>
         </div>
-        <div class="absolute start-0 top-1/2 -translate-y-[75px] md:-translate-y-[60px] -translate-x-[50px] md:-translate-x-[70px] bg-gray-300 hover:bg-gray-200 border dark:border-gray-500 rounded" data-glide-el="controls">
+        <div class="leading-0 p-[2px] absolute start-0 -translate-x-[24px] sm:-translate-x-[30px] md:-translate-x-[50px] md:-translate-x-[20px] border bg-gray-300/80 hover:bg-gray-200 dark:border-gray-500 rounded-full" data-glide-el="controls"
+             :style="{top: topValue}">
             <button data-glide-dir="<">
-                <svg class="w-[30px] h-[30px] md:w-[40px] md:h-[40px]" viewBox="0 0 24 24" fill="currentColor" role="img" style="display:inline-block;vertical-align:middle" xmlns="http://www.w3.org/2000/svg"><g fill="currentColor"><path fill="currentColor" d="M16 6a1 1 0 0 0-1.6-.8l-8 6a1 1 0 0 0 0 1.6l8 6A1 1 0 0 0 16 18V6Z"/></g></svg>
+                <svg class="w-[40px] h-[40px] md:w-[25px] md:h-[25px] lg:w-[30px] lg:h-[30px]" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg"><path d="M30.83 32.67l-9.17-9.17 9.17-9.17-2.83-2.83-12 12 12 12z"/><path d="M0-.5h48v48h-48z" fill="none"/></svg>
             </button>
         </div>
-        <div class="absolute end-0 top-1/2 -translate-y-[75px] md:-translate-y-[60px] translate-x-[50px] md:translate-x-[70px] bg-gray-300 hover:bg-gray-200 border dark:border-gray-500 rounded" data-glide-el="controls">
+        <div class="leading-0 p-[2px] absolute end-0 translate-x-[24px] sm:translate-x-[30px] md:translate-x-[50px] md:translate-x-[20px] border bg-gray-300/80 hover:bg-gray-200 dark:border-gray-500 rounded-full shadow" data-glide-el="controls"
+             :style="{top: topValue}">
             <button data-glide-dir=">">
-                <svg class="w-[30px] h-[30px] md:w-[40px] md:h-[40px]" viewBox="0 0 24 24" fill="currentColor" role="img" style="display:inline-block;vertical-align:middle" xmlns="http://www.w3.org/2000/svg"><g fill="currentColor"><path fill="currentColor" d="M8 6a1 1 0 0 1 1.6-.8l8 6a1 1 0 0 1 0 1.6l-8 6A1 1 0 0 1 8 18V6Z"/></g></svg>
+                <svg class="w-[40px] h-[40px] md:w-[25px] md:h-[25px] lg:w-[30px] lg:h-[30px]" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg"><path d="M17.17 32.92l9.17-9.17-9.17-9.17 2.83-2.83 12 12-12 12z"/><path d="M0-.25h48v48h-48z" fill="none"/></svg>
             </button>
         </div>
     </div>
 </div>
+<script>
+    initializeCarousel = function () {
+        const calculateHeight = function () {
+            return (document.getElementsByClassName('carousel-image')[0].height - 20) / 2 + 'px';
+        }
+
+        return {
+            calculateHeight: calculateHeight,
+            topValue: calculateHeight(),
+        }
+    }
+</script>
