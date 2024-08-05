a2c4eabe5a22eb8be3ce8a53105048e10ef86886
michalkaminski <m.kaminski@clawrock.com>
2024-08-05 20:50:44 +0200

    Add message.js file to vite configuration

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
             'resources/css/app.css',
             'resources/js/app.js',
             'resources/js/carousel.js',
+            'resources/js/message.js'
         ]),
         mkcert({
             savePath: './certs'
