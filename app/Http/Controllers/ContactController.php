748aa991ecd00122cd0dd3e4469133b4d04760f6
michalkaminski <m.kaminski@clawrock.com>
2024-08-08 20:43:19 +0200

    Create controller

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Mail\ContactMessage;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+
+class ContactController extends Controller
+{
+    public function edit(): View
+    {
+        return view('contact');
+    }
+
+    public function store(Request $request): \Illuminate\Http\RedirectResponse
+    {
+        $request->validate([
+            'first-name' => 'required',
+            'last-name' => 'required',
+            'user-email' => 'required|email',
+            'message' => 'required',
+        ]);
+
+        $details = $request->all();
+        Mail::send(new ContactMessage([
+            'firstName' => $details['first-name'],
+            'lastName' => $details['last-name'],
+            'company' => $details['company'],
+            'userEmail' => $details['user-email'],
+            'message' => $details['message'],
+        ]));
+
+        return redirect()->route('contact.edit')->with('success', 'Your message has been sent successfully. We will contact you soon.');
+    }
+}
