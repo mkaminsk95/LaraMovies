bb4f8a360e34a6d2b42a312c0c41ba2f2ac4c860
michalkaminski <m.kaminski@clawrock.com>
2024-08-08 20:39:16 +0200

    Upgrade form page

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,27 +4,29 @@
             <h2 class="text-3xl font-bold tracking-tight text-gray-400 sm:text-4xl">Contact us</h2>
             <p class="mt-2 text-lg leading-8 text-gray-600">Tell us about your ideas, concerns, or anything else you'd like to share.<br/>We're here to listen and help!</p>
         </div>
-        <form action="#" method="POST" class="mx-auto mt-16 max-w-xl sm:mt-20">
+        <form action="/contact" method="POST" class="mx-auto mt-16 max-w-xl sm:mt-20">
+            <x-action-status-message/>
+            @csrf
             <div class="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
                 <div>
                     <x-input-label for="first-name">First name</x-input-label>
-                    <x-text-input id="first-name" name="first-name" autocomplete="given-name"/>
+                    <x-text-input id="first-name" name="first-name" autocomplete="given-name" required/>
                 </div>
                 <div>
                     <x-input-label for="first-name">Last name</x-input-label>
-                    <x-text-input id="last-name" name="last-name" autocomplete="family-name"/>
+                    <x-text-input id="last-name" name="last-name" autocomplete="family-name" required/>
                 </div>
                 <div class="sm:col-span-2">
                     <x-input-label for="company">Company</x-input-label>
                     <x-text-input name="company" id="company" autocomplete="organization"/>
                 </div>
                 <div class="sm:col-span-2">
-                    <x-input-label for="email">Email</x-input-label>
-                    <x-text-input name="email" id="email" autocomplete="email"/>
+                    <x-input-label for="user-email">Email</x-input-label>
+                    <x-text-input name="user-email" id="user-email" autocomplete="email" type="email" required/>
                 </div>
                 <div class="sm:col-span-2">
-                    <x-input-label for="company">Message</x-input-label>
-                    <x-textarea-input rows="4" name="message" id="message"/>
+                    <x-input-label for="message">Message</x-input-label>
+                    <x-textarea-input rows="4" name="message" id="message" required/>
                 </div>
 
             </div>
@@ -33,5 +35,4 @@
             </div>
         </form>
     </div>
-
 </x-layout>
