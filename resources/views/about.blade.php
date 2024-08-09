64e4cc193e3a44262429cf67351c099532995ce3
michalkaminski <m.kaminski@clawrock.com>
2024-08-09 09:28:19 +0200

    Create about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,9 +1,20 @@
 <x-layout>
-    <section class="text-2xl text-gray-600 body-font">
-        <div class="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
-            <div class="text-center lg:w-2/3 w-full">
-                <p class="mb-8 leading-relaxed">About page.</p>
+    <div class="sm:flex sm:flex-row justify-center mt-10 items-center mx-auto max-w-screen-xl">
+        <div class="sm:w-fit p-10">
+            <div class="image object-center text-center">
+                <img src="{{ Vite::asset('resources/assets/images/about_image.png') }}" alt="Illustration">
             </div>
         </div>
-    </section>
+        <div class="sm:w-1/2 p-5">
+            <div class="text">
+                <h2 class="my-4 font-bold text-3xl sm:text-4xl dark:text-gray-300">About <span class="text-indigo-600">Our Company</span>
+                </h2>
+                <p class="text-gray-700">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, commodi
+                    doloremque, fugiat illum magni minus nisi nulla numquam obcaecati placeat quia, repellat tempore
+                    voluptatum.
+                </p>
+            </div>
+        </div>
+    </div>
 </x-layout>
