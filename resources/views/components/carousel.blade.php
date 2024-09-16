11469d6feb4f93e9553d2b630dab216d95d81479
michalkaminski <m.kaminski@clawrock.com>
2024-09-16 10:54:39 +0200

    Increase arrow size in carousels

--- resources/views/components/carousel.blade.php
+++ resources/views/components/carousel.blade.php
@@ -7,14 +7,14 @@
                 {{ $slot }}
             </ul>
         </div>
-        <div class="leading-0 p-[2px] absolute {{ $arrowsPosition }} left-0 -translate-x-[20px] sm:-translate-x-[40px] md:-translate-x-[42px] md:-translate-x-[20px] border bg-gray-300/10 hover:bg-gray-200 dark:border-gray-500/40 rounded" data-glide-el="controls">
+        <div class="leading-0 p-[2px] absolute {{ $arrowsPosition }} left-0 -translate-x-[20px] sm:-translate-x-[40px] md:-translate-x-[42px] md:-translate-x-[20px]" data-glide-el="controls">
             <button data-glide-dir="<">
-                <svg fill="#AAAAAA" class="w-[40px] h-[40px] md:w-[25px] md:h-[25px] lg:w-[30px] lg:h-[30px]" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg"><path d="M30.83 32.67l-9.17-9.17 9.17-9.17-2.83-2.83-12 12 12 12z"/><path d="M0-.5h48v48h-48z" fill="none"/></svg>
+                <svg fill="#AAAAAA" class="w-[40px] h-[40px] md:w-[25px] md:h-[25px] lg:w-[40px] lg:h-[40px]" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg"><path d="M30.83 32.67l-9.17-9.17 9.17-9.17-2.83-2.83-12 12 12 12z"/><path d="M0-.5h48v48h-48z" fill="none"/></svg>
             </button>
         </div>
-        <div class="leading-0 p-[2px] absolute {{ $arrowsPosition }} right-0 translate-x-[20px] sm:translate-x-[30px] md:translate-x-[42px] md:translate-x-[20px] border bg-gray-300/10 hover:bg-gray-200 dark:border-gray-500/40 rounded shadow" data-glide-el="controls">
+        <div class="leading-0 p-[2px] absolute {{ $arrowsPosition }} right-0 translate-x-[20px] sm:translate-x-[30px] md:translate-x-[42px] md:translate-x-[20px]" data-glide-el="controls">
             <button data-glide-dir=">">
-                <svg fill="#AAAAAA" class="w-[40px] h-[40px] md:w-[25px] md:h-[25px] lg:w-[30px] lg:h-[30px]" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg"><path d="M17.17 32.92l9.17-9.17-9.17-9.17 2.83-2.83 12 12-12 12z"/><path d="M0-.25h48v48h-48z" fill="none"/></svg>
+                <svg fill="#AAAAAA" class="w-[40px] h-[40px] md:w-[25px] md:h-[25px] lg:w-[40px] lg:h-[40px]" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg"><path d="M17.17 32.92l9.17-9.17-9.17-9.17 2.83-2.83 12 12-12 12z"/><path d="M0-.25h48v48h-48z" fill="none"/></svg>
             </button>
         </div>
     </div>
