74f87fb9bd888ecf8cab5b7dba568087ee798729
michalkaminski <m.kaminski@clawrock.com>
2024-09-17 14:08:09 +0200

    Decrease margin for mobile view

--- resources/views/movies/partials/poster-magnified.blade.php
+++ resources/views/movies/partials/poster-magnified.blade.php
@@ -1,7 +1,7 @@
 <div x-show="togglePoster"
      x-transition.opacity.duration.400ms
      @click="togglePoster = false" class="fixed inset-0 bg-gray-500/75 z-50">
-    <img @click.stop class="w-[400px] mx-auto mt-[120px] flex-none"
+    <img @click.stop class="w-[400px] mx-auto mt-[50px] mt-[80px] sm:mt-[120px] flex-none"
          src="https://image.tmdb.org/t/p/w780{{ $movie['poster_path'] }}"
          alt="{{ __('poster') }}">
 </div>
