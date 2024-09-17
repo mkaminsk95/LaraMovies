5f8c40c3f3cc20c0684645fe8e4929630e579f5c
michalkaminski <m.kaminski@clawrock.com>
2024-09-17 14:03:10 +0200

    Add magnification option to poster

--- resources/views/movies/partials/movie-body.blade.php
+++ resources/views/movies/partials/movie-body.blade.php
@@ -1,12 +1,24 @@
 @php
     $locale = app()->getLocale();
 @endphp
-<div class="flex flex-row">
+<div x-data="{togglePoster: false, toggleMagnifierIcon: false}" class="flex flex-row">
+    @include('movies.partials.poster-magnified')
+
     <div class="w-[80px] min-[500px]:w-[110px] sm:w-[200px] sm:min-w-[80px] sm:max-w-[200px] md:max-w-full lg:basis-1/4">
         @if($movie['poster_path'] !== null)
-            <img class="flex-none bg-gray-50 rounded"
-                 src="https://image.tmdb.org/t/p/w780{{ $movie['poster_path'] }}"
-                 alt="{{ __('poster') }}">
+            <div @click="togglePoster = true"
+                 @mouseenter="toggleMagnifierIcon = true"
+                 @mouseleave="toggleMagnifierIcon = false"
+                 class="relative">
+                <div x-show="toggleMagnifierIcon"
+                     x-transition.opacity.duration.400ms
+                     class="transition h-full w-full absolute bg-gray-300/50 flex items-center justify-center">
+                        <svg class="w-14" viewBox="0 0 32 32" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="M27.414,24.586l-5.077-5.077C23.386,17.928,24,16.035,24,14c0-5.514-4.486-10-10-10S4,8.486,4,14  s4.486,10,10,10c2.035,0,3.928-0.614,5.509-1.663l5.077,5.077c0.78,0.781,2.048,0.781,2.828,0  C28.195,26.633,28.195,25.367,27.414,24.586z M7,14c0-3.86,3.14-7,7-7s7,3.14,7,7s-3.14,7-7,7S7,17.86,7,14z" id="XMLID_223_"/></svg>
+                </div>
+                <img class="flex-none bg-gray-50 rounded"
+                     src="https://image.tmdb.org/t/p/w780{{ $movie['poster_path'] }}"
+                     alt="{{ __('poster') }}">
+            </div>
         @else
             <img src="https://dummyimage.com/780x1170/d2d5db/fff&text=+" alt="{{ __('poster') }}">
         @endif
