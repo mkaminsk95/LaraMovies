074b46342765125c26f2a1df5b8bbdd670abec21
mkaminsk95 <43847416+mkaminsk95@users.noreply.github.com>
2024-10-10 14:10:37 +0200

    Modify CI/CD script, add tests and fix Factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -30,7 +30,7 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
-            'avatar_id' => Avatar::get()->random()->id,
+            'avatar_id' => Avatar::get()->isNotEmpty() ? Avatar::get()->random()->id : null,
             'is_admin' => false,
             'is_dummy' => true,
         ];
