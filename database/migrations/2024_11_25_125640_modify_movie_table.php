60d371163403bf971960e28010ace5f70554a79b
michalkaminski <m.kaminski@clawrock.com>
2024-11-25 15:48:19 +0100

    Add new migration

---
+++ database/migrations/2024_11_25_125640_modify_movie_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('movies', function (Blueprint $table) {
+            $table->float('vote_average', 7, 5, false)->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('movies', function (Blueprint $table) {
+            $table->double('vote_average', 7, 6, false)->nullable()->change();
+        });
+    }
+};
